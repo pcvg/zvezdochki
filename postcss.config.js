bea64e1b275c90107906e1b2bbb195b9c3524586
Dima Lunkin <timonbandit@gmail.com>
2020-02-04 17:21:57 +0300

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-flexbugs-fixes': {},
+    'postcss-csso': {},
+    'postcss-preset-env': {
+      autoprefixer: true,
+      stage: 3
+    }
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+class StarRating {
+  constructor(props) {
+    console.log(props); //eslint-disable-line
+  }
+
+  init() {
+
+  }
+}
+
+let starRating = new StarRating("test");
+
+starRating.init();
